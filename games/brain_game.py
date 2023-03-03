efa597027298d36b1887e3fec49e6726051400b0
alexanderZaych <118917232+Alexander-Zaychenko@users.noreply.github.com>
2023-03-03 19:38:05 +0000

    all save

---
+++ games/brain_game.py
@@ -0,0 +1,9 @@
+from random import randint
+
+def engine():
+    pass
+def in_gme()  :
+    first_num = randin(1100, )
+    second_name = randint(1, 100 )
+
+
--- games/rock_scissors_paper.py
+++ games/rock_scissors_paper.py
@@ -1,15 +1,18 @@
 from random import randint
 
 def engine(computer_choice, player_choice):
-    if computer_choice == player_choice:
-        return "Ничья! "
-    elif computer_choice == 1 and player_choice == 2 or computer_choice == 2 and player_choice == 3 or computer_choice == 3 and player_choice == 1:
-        return "Поражение! "
-    elif computer_choice == 2 and player_choice == 1 or computer_choice == 3 and player_choice == 2 or computer_choice == 1 and player_choice == 3:
-        return "Победа! "
+    if player_choice < 4 and player_choice > 0:
+      if computer_choice == player_choice:
+          return "Ничья! "
+      elif computer_choice == 1 and player_choice == 2 or computer_choice == 2 and player_choice == 3 or computer_choice == 3 and player_choice == 1:
+          return "Поражение! "
+      elif computer_choice == 2 and player_choice == 1 or computer_choice == 3 and player_choice == 2 or computer_choice == 1 and player_choice == 3:
+          return "Победа! "
+    else:
+        return "Ошибка! "
 
   
-def game(player_choice):
+def in_game(player_choice):
     computer_choice = randint(1, 3)
-    engine(computer_choice, player_choice)
+    return engine(computer_choice, int(player_choice))
   
